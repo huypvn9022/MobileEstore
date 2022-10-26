3708b30fae6f86d2b039f520329072afc1a28ae9
Tai <tientai9a9@gmail.com>
2022-10-26 21:52:37 +0700

    add file donhang.java

--- src/main/java/com/mobilestore/model/DonHang.java
+++ src/main/java/com/mobilestore/model/DonHang.java
@@ -1,5 +1,26 @@
 package com.mobilestore.model;
 
-public class DonHang {
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+@Entity
+@Table(name="DonHang")
+public class DonHang {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int maDon;
+	private String maKH;
+	private String maNV;
+	private Date ngayTao;
+	private String trangThai;
 }
