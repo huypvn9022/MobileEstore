2b15f5a495108ca6c205872d4eab03be69ed60ab
Nhat Huy <phanhuy9022@gmail.com>
2022-10-26 20:41:35 +0700

    Add file LoaiSP.java

--- src/main/java/com/mobilestore/model/LoaiSanPham.java
+++ src/main/java/com/mobilestore/model/LoaiSanPham.java
@@ -1,6 +1,18 @@
 package com.mobilestore.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "LoaiSP")
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoaiSanPham {
-	
+	private String maLoai;
+	private String tenLoai;
 }
