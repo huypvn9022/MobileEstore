21d21ceb1e98bd743ac4527f1851069d8ae1dea9
Hung <tranhuyhung139@gmail.com>
2022-10-26 21:43:29 +0700

    add file KH vs HA

--- src/main/java/com/mobilestore/model/KhachHang.java
+++ src/main/java/com/mobilestore/model/KhachHang.java
@@ -1,5 +1,22 @@
 package com.mobilestore.model;
 
-public class KhachHang {
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="KhachHang")
+public class KhachHang {
+	@Id
+	private String TaiKhoan;
+	private String MatKhau;
+	private String HoTen;
+	private String Email;
+	private int SDT;
+	private String DiaChi;
 }
