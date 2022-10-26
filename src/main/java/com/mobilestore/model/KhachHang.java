1fd09c6e3598e5fe3c176448b9c98ebc40473e9c
Tai <tientai9a9@gmail.com>
2022-10-26 21:37:20 +0700

    add file khachhang.java

--- src/main/java/com/mobilestore/model/KhachHang.java
+++ src/main/java/com/mobilestore/model/KhachHang.java
@@ -1,5 +1,22 @@
 package com.mobilestore.model;
 
-public class KhachHang {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+@Entity
+@Table(name = "KhachHang")
+public class KhachHang {
+	@Id
+	private String taiKhoan;
+	private String matKhau;
+	private String hoTen;
+	private String email;
+	private String sdt;
+	private String diaChi;
 }
