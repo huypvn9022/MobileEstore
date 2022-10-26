22d7ca82d1dd1bd2a941956520cd9a927a07d06e
Hung <tranhuyhung139@gmail.com>
2022-10-26 21:33:25 +0700

    add file NhanVien

--- src/main/java/com/mobilestore/model/NhanVien.java
+++ src/main/java/com/mobilestore/model/NhanVien.java
@@ -1,5 +1,26 @@
 package com.mobilestore.model;
 
-public class NhanVien {
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="NhanVien")
+public class NhanVien {
+	@Id
+	private String TaiKhoan;
+	private String MatKhau;
+	private String HoTen;
+	private String Email;
+	private int SDT;
+	private String DiaChi;
+	private Date NgaySinh;
+	private boolean ChucVu;
+	
+	
 }
