424dae814a01146502049deab70fd95713d13ce2
hectorphan-bed <phanhuy9022@gmail.com>
2022-10-29 17:04:02 +0700

    config layout use thymeleaf

--- src/main/java/com/mobilestore/controller/HomeController.java
+++ src/main/java/com/mobilestore/controller/HomeController.java
@@ -1,5 +1,15 @@
 package com.mobilestore.controller;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/index")
+	public String index(Model model) {
+		return "layout/index";
+	}
+	
 }
--- src/main/java/com/mobilestore/model/KhachHang.java
+++ src/main/java/com/mobilestore/model/KhachHang.java
@@ -1,6 +1,5 @@
 package com.mobilestore.model;
 
-import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Entity;
