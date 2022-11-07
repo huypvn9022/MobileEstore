e1f6d9e39259e87839f93a26ff44d74c50b9ced9
hectorphan-bed <d.bed23@gsoft01.tk>
2022-11-07 20:49:26 +0700

    update file giao dien

--- src/main/java/com/mobilestore/dao/ChiTietDonHangDAO.java
+++ src/main/java/com/mobilestore/dao/ChiTietDonHangDAO.java
@@ -2,7 +2,7 @@ package com.mobilestore.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.mobilestore.model.ChiTietDonHang;
+import com.mobilestore.entity.ChiTietDonHang;
 
 public interface ChiTietDonHangDAO extends JpaRepository<ChiTietDonHang, Integer>{
 
--- src/main/java/com/mobilestore/dao/KhachHangDAO.java
+++ src/main/java/com/mobilestore/dao/KhachHangDAO.java
@@ -2,7 +2,7 @@ package com.mobilestore.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.mobilestore.model.KhachHang;
+import com.mobilestore.entity.KhachHang;
 
 public interface KhachHangDAO extends JpaRepository<KhachHang, String>{
 
