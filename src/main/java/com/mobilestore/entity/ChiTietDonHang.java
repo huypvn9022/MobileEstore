a3e7ca38aee6fe18af4bc9e548d487abcb6f2e4a
hectorphan-bed <d.bed23@gsoft01.tk>
2022-11-09 21:35:13 +0700

    fix error table san

--- src/main/java/com/mobilestore/entity/ChiTietDonHang.java
+++ src/main/java/com/mobilestore/entity/ChiTietDonHang.java
@@ -17,7 +17,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "ChiTietDH")
+@Table(name = "ChitietDH")
 public class ChiTietDonHang {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/mobilestore/entity/SanPham.java
+++ src/main/java/com/mobilestore/entity/SanPham.java
@@ -24,7 +24,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "SanPham")
+@Table(name = "Sanpham")
 public class SanPham implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
