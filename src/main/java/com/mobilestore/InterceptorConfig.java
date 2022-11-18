27218a465abf1a69e14d3b2f331103d78dd1388f
Cosker <d.bed25@gsoft01.tk>
2022-11-18 21:51:30 +0700

    update file index.html

--- src/main/java/com/mobilestore/InterceptorConfig.java
+++ src/main/java/com/mobilestore/InterceptorConfig.java
@@ -1,17 +1,9 @@
 package com.mobilestore;
 
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 import com.mobilestore.interceptor.GlobalInterceptor;
 
@@ -24,7 +16,7 @@ public class InterceptorConfig implements WebMvcConfigurer{
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(globalInterceptor)
 			.addPathPatterns("/**")
-				.excludePathPatterns("/admin/**", "/rest/**", "/assets/**");
+			.excludePathPatterns("/admin/**", "/rest/**", "/assets/**");
 	}
 	
 }
