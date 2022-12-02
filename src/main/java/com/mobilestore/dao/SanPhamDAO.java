c27f551f23aef868b9d11e39ea67d5c3c6a674f0
Hung <tranhuyhung139@gmail.com>
2022-12-02 19:58:56 +0700

    addfile

--- src/main/java/com/mobilestore/dao/SanPhamDAO.java
+++ src/main/java/com/mobilestore/dao/SanPhamDAO.java
@@ -15,4 +15,65 @@ public interface SanPhamDAO extends JpaRepository<SanPham, Integer>{
 	
 	@Query(value = "SELECT o FROM SanPham o WHERE o.tenSP LIKE ?1")
 	Page <SanPham> findAllByKeywords(Pageable pageable, String keywords);
+	
+	@Query(value = "SELECT o FROM SanPham o WHERE o.donGia  BETWEEN '0' and '000000' ")
+	Page <SanPham> findByDongia(Pageable pageable, Integer donGia);
+	@Query(value = "SELECT o FROM SanPham o WHERE o.donGia  BETWEEN '2000000' and '4000000' ")
+	Page <SanPham> findByDongia1(Pageable pageable, Integer donGia);
+	@Query(value = "SELECT o FROM SanPham o WHERE o.donGia  BETWEEN '4000000' and '7000000' ")
+	Page <SanPham> findByDongia2(Pageable pageable, Integer donGia);
+	@Query(value = "SELECT o FROM SanPham o WHERE o.donGia  BETWEEN '7000000' and '13000000' ")
+	Page <SanPham> findByDongia3(Pageable pageable, Integer donGia);
+	@Query(value = "SELECT o FROM SanPham o WHERE o.donGia  BETWEEN '13000000' and '20000000' ")
+	Page <SanPham> findByDongia4(Pageable pageable, Integer donGia);
+	
+	@Query(value = "SELECT o FROM SanPham o Order BY o.donGia  ")
+	Page <SanPham> findByDongia5(Pageable pageable, Integer donGia);
+	
+	@Query(value = "SELECT o FROM SanPham o Order BY o.donGia  DESC")
+	Page <SanPham> findByDongia6(Pageable pageable, Integer donGia);
+	
+	@Query(value = "select o from SanPham o where o.maCH.ram like '4 GB'")
+	Page <SanPham> findAllByRam(Pageable pageable);
+	@Query(value = "select o from SanPham o where o.maCH.ram like '6 GB'")
+	Page <SanPham> findAllByRam1(Pageable pageable);
+	@Query(value = "select o from SanPham o where o.maCH.ram like '8 GB'")
+	Page <SanPham> findAllByRam2(Pageable pageable);
+	
+	@Query(value = "select o from SanPham o where o.maCH.rom like '32 GB'")
+	Page <SanPham> findAllByRom(Pageable pageable);
+	@Query(value = "select o from SanPham o where o.maCH.rom like '64 GB'")
+	Page <SanPham> findAllByRom1(Pageable pageable);
+	@Query(value = "select o from SanPham o where o.maCH.rom like '128 GB'")
+	Page <SanPham> findAllByRom2(Pageable pageable);
+	
+	@Query(value = "select o from SanPham o where o.maCH.heDH like 'iPhone (iOS)'")
+	Page <SanPham> findAllByHDH(Pageable pageable);
+	@Query(value = "select o from SanPham o where o.maCH.heDH like 'Android'")
+	Page <SanPham> findAllByHDH1(Pageable pageable);
+
+	@Query(value = "SELECT o FROM SanPham o WHERE o.maHang.maHang = 'IP'")
+	Page<SanPham> findAllByMaHang1(Pageable pageable);
+	@Query(value = "SELECT o FROM SanPham o WHERE o.maHang.maHang = 'HW'")
+	Page<SanPham> findAllByMaHang2(Pageable pageable);
+	@Query(value = "SELECT o FROM SanPham o WHERE o.maHang.maHang = 'NK'")
+	Page<SanPham> findAllByMaHang3(Pageable pageable);
+	@Query(value = "SELECT o FROM SanPham o WHERE o.maHang.maHang = 'OP'")
+	Page<SanPham> findAllByMaHang4(Pageable pageable);
+	@Query(value = "SELECT o FROM SanPham o WHERE o.maHang.maHang = 'RM'")
+	Page<SanPham> findAllByMaHang5(Pageable pageable);
+	@Query(value = "SELECT o FROM SanPham o WHERE o.maHang.maHang = 'SN'")
+	Page<SanPham> findAllByMaHang6(Pageable pageable);
+	@Query(value = "SELECT o FROM SanPham o WHERE o.maHang.maHang = 'SS'")
+	Page<SanPham> findAllByMaHang7(Pageable pageable);
+	@Query(value = "SELECT o FROM SanPham o WHERE o.maHang.maHang = 'VV'")
+	Page<SanPham> findAllByMaHang8(Pageable pageable);
+	@Query(value = "SELECT o FROM SanPham o WHERE o.maHang.maHang = 'XM'")
+	Page<SanPham> findAllByMaHang9(Pageable pageable);
+	
+	@Query(value = "SELECT o FROM SanPham o WHERE o.maLoai.maLoai = 'DT'")
+	Page<SanPham> findAllByMaLoai(Pageable pageable);
+	
+	@Query(value = "SELECT o FROM SanPham o WHERE o.maLoai.maLoai = 'PK'")
+	Page<SanPham> findAllByMaLoai1(Pageable pageable);
 }
