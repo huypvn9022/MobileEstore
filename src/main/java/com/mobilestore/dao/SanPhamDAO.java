1226d323460cc43374d6ce4d584b58bfaebe6b50
Hung <tranhuyhung139@gmail.com>
2022-12-19 20:16:52 +0700

    add

--- src/main/java/com/mobilestore/dao/SanPhamDAO.java
+++ src/main/java/com/mobilestore/dao/SanPhamDAO.java
@@ -12,42 +12,43 @@ import com.mobilestore.model.SanPham;
 public interface SanPhamDAO extends JpaRepository<SanPham, Integer>{
 	
 
-	@Query(value = "SELECT o FROM SanPham o WHERE o.tenSP LIKE ?1")
+	@Query(value = "SELECT o FROM SanPham o WHERE o.daXoa = 0 and o.tenSP LIKE ?1")
 	List <SanPham> findAllByKeyword(String keywords);
 	
-	
+	@Query(value = "SELECT o FROM SanPham o WHERE o.daXoa = 0 ")
+	List <SanPham> findAllSPDelete();
 
 	
-	@Query(value = "SELECT o FROM SanPham o WHERE o.mahang.maHang like ?1")
+	@Query(value = "SELECT o FROM SanPham o WHERE o.daXoa = 0 and o.mahang.maHang like ?1")
 	Page<SanPham> findAllByMaHang(String mahang,Pageable pageable);
 	
-	@Query(value = "SELECT o FROM SanPham o WHERE o.tenSP LIKE ?1")
+	@Query(value = "SELECT o FROM SanPham o WHERE o.daXoa = 0 and o.tenSP LIKE ?1")
 	Page <SanPham> findAllByKeywords(Pageable pageable, String keywords);
 		
-	@Query(value = "SELECT o FROM SanPham o WHERE o.donGia  BETWEEN ?1 and ?2 ")
+	@Query(value = "SELECT o FROM SanPham o WHERE o.daXoa = 0 and o.donGia  BETWEEN ?1 and ?2 ")
 	Page <SanPham> findByDongia(Pageable pageable , Double  min, Double  max);
 			
-	@Query(value = "select o from SanPham o where o.mach.ram like ?1 ")
+	@Query(value = "select o from SanPham o where o.daXoa = 0 and o.mach.ram like ?1 ")
 	Page <SanPham> findAllByRam(Pageable pageable,String ram);
 	
-	@Query(value = "select o from SanPham o where o.mach.rom like ?1")
+	@Query(value = "select o from SanPham o where o.daXoa = 0 and o.mach.rom like ?1")
 	Page <SanPham> findAllByRom(Pageable pageable,String rom);
 
 	
-	@Query(value = "select o from SanPham o where o.mach.heDH like ?1")
+	@Query(value = "select o from SanPham o where o.daXoa = 0 and o.mach.heDH like ?1")
 	Page <SanPham> findAllByHDH(Pageable pageable,String hedh);
 
-	@Query(value = "SELECT o FROM SanPham o WHERE o.maloai.maLoai like ?1")
+	@Query(value = "SELECT o FROM SanPham o WHERE o.daXoa = 0 and o.maloai.maLoai like ?1")
 	Page<SanPham> findAllByMaLoai2(Pageable pageable,String maloai);
 
 
 
-	@Query(value = "SELECT o FROM SanPham o WHERE o.maloai.maLoai like 'DT'")
+	@Query(value = "SELECT o FROM SanPham o WHERE o.daXoa = 0 and o.maloai.maLoai like 'DT'")
 	Page<SanPham> findAllByMaLoai(Pageable pageable);
 
 
 
-	@Query(value = "SELECT o FROM SanPham o WHERE o.maloai.maLoai like 'PK'")
+	@Query(value = "SELECT o FROM SanPham o WHERE  o.daXoa = 0 and o.maloai.maLoai like 'PK'")
 	Page<SanPham> findAllByMaLoai1(Pageable pageable);
 	
 
